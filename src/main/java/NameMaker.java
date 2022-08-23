43678b292898b7e8443ee1d5c7c40493a512d77d
Thomas Czachorowski <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-08-23 08:38:03 -0400

    Initial commit

---
+++ src/main/java/NameMaker.java
@@ -0,0 +1,17 @@
+
+
+public class NameMaker {
+
+
+    public static void main(String args[])
+    {
+        String firstName="Bryan",
+                middleName="White",
+                lastName="Cranston",
+                fullName;
+
+        fullName=firstName+" "+middleName+" "+lastName;
+        System.out.println(fullName);
+    }
+
+}
